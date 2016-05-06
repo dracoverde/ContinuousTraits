394013924d840e2ffd5b1d93ac705f35e2beea81
Ryan Roney <Ryan Roney>
2016-05-06 13:37:13 -0400

    adding functions

--- ContinuousFunctions.R
+++ ContinuousFunctions.R
@@ -3,7 +3,9 @@ library(geiger) #utilty fns
 library(OUWie)
 
 #You can use code you wrote for the correlation exercise here.
+discrete.data <- rTraitDisc(tree, model = "ER", k=2, states=c(0,1))
 
+continuous.data <- rTraitCont(tree, model="BM")
 
 VisualizeData <- function(phy, data) {
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
@@ -11,6 +13,7 @@ VisualizeData <- function(phy, data) {
 
 CleanData <- function(phy, data) {
 	#treedata() in Geiger is probably my favorite function in R.
+  treedata(phy, data, sort=FALSE, warnings=TRUE)
 }
 
 RunSingleOUwieModel<-function(model, phy, data) {
